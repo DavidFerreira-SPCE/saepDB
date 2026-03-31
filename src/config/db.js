710c8b296c9bb795dbf7712ecc903c694cee9c86
David <rwesley2000@gmail.com>
2026-03-31 11:25:54 -0300

    Inicio do BackEnd - feat. v0.1

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "db_restauranteSAEP",
+  password: "wcc@2023",
+  port: 5432,
+});
+
+pool
+  .connect()
+  .then(() => console.log("Conectado ao postgreSQL"))
+  .catch((err) => console.error("erro na conexão", err));
+
+module.exports = pool;
